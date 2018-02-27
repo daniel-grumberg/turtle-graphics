85077f56792f350cd5a0d0df3904eb68138bf9a6
DANIEL GRUMBERG <dany.grumberg@gmail.com>
2018-02-27 22:52:39 +0000

    Add simple rhombus shape for usage in worksheet

---
+++ projects/rhombus.py
@@ -0,0 +1,18 @@
+#! /usr/local/bin/python3
+from turtle import *
+from datetime import datetime
+
+mode("logo")
+speed(0)
+
+right(45)
+forward(30)
+right(45)
+forward(50)
+right(135)
+forward(30)
+right(45)
+forward(50)
+
+done()
+
